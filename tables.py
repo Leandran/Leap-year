a09d643314619fac5c32a2d15ab5a6a69e3baabe
Leandran <41592532+Leandran@users.noreply.github.com>
2020-11-30 01:34:54 +0200

    Delete tables.py

--- tables.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Enter any number for the multiplication table: "))
-print("The multiplication table for " + str(num) + " is ")
-for i in range(1,13):
-        print(num, 'x' , i, '=' ,num*i)
-         
